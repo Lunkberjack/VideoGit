c760afe4c5741dae92222f6e0b18fd0e85e3098c
Lunkberjack <l.le.mi.lunk15@gmail.com>
2022-03-19 12:42:11 +0100

    Cambio en rama testing

--- src/videogit/HolaMundo.java
+++ src/videogit/HolaMundo.java
@@ -3,7 +3,9 @@ package videogit;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// Este comentario se ha aÃ±adido desde GitHub
+		// Este comentario se ha añadido desde GitHub
 		System.out.println("Hola Mundo");
+		// Esto solo aparecerá en la rama testing
+		System.out.println("Test");
 	}
 }
